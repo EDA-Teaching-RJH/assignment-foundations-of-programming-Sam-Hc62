9a126309e34c2a2884f77b9c270d9c955644bdba
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-13 15:38:53 +0000

    change 6

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,33 +1,38 @@
 def main():
     def init_database():
-        names = ["Picard", "Spock", "Worf", "Data", "Sisko"]
-        ranks = ["Captain", "Commander", "Lieutenant", "Lieutenant Commander", "Captain"]
-        divs  = ["Command", "Science", "Security", "Operations", "Command"]
-        IDs = ["SP-937-215","S179-276SP","WORF-201","D-4C-6F","FS-326-758"]
-        return names, ranks, divs, IDs
-    init_database()
+        names = ["Picard", "Riker", "Worf", "Data", "Troi"]
+        ranks = ["Captain", "Commander", "Lieutenant", "Lieutenant Commander", "Lieutenant Commander"]
+        divs  = ["Command", "Command", "Security", "Operations", "Counselor"]
+        IDs = ["PC-1701-D", "RI-1701-D", "WORF-201", "D-4C-6F", "TR-1701-D"]
+        return names, ranks, divs, IDs 
+    names, ranks, divs, IDs = init_database()
     def display_menu():
         user_name = input("What is your full name?")
         print("Student logged in:", user_name.title())
-        x = input("Choose an option:\n1: Add member\n2: Remove member\n3: Update rank\n4: Display roster\n5:Search crew member\n6: Filter by division\n7: Calculate payroll\n8: Count Officers\n")
+        x = int(input("Choose an option:\n1: Add member\n2: Remove member\n3: Update rank\n4: Display roster\n5:Search crew member\n6: Filter by division\n7: Calculate payroll\n8: Count Officers\n"))
         try:
-            if x == 1:
-                return 1
-            elif x == 2:
-                return 2
-            elif x == 3:
-                return 3
-            elif x == 4:
-                return 4
-            elif x == 5:
-                return 5
-            elif x == 6:
-                return 6
-            elif x == 7:
-                return 7
-            elif x == 8:
-                return 8
+            if 1 <= x <= 8:
+                return x
         except ValueError:
             print("not an option, try again")
     display_menu()
+    def add_member():
+        TNG_ranks = ["Crewman","Cadet","Ensign","Lieutenant Junior Grade","Lieutenant","Lieutenant Commander","Commander","Captain","Rear Admiral","Vice Admiral","Admiral","Fleet Admiral"]
+        while True:
+            member_rank = input("Rank:")
+            if member_rank not in TNG_ranks:
+                print("Not a valid rank")
+                continue
+            member_ID = input("ID:")
+            if member_ID in IDs:
+                print("ID already belongs to someone")
+                continue        
+            member_name = input("Name:")
+            member_div = input("Division:")
+            ranks.append(member_rank)
+            IDs.append(member_ID)
+            names.append(member_name)
+            divs.append(member_div)
+            break
+    add_member()
 main()
